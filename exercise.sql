3e0c08e787888967ade6d6a327f39ffe4403e1b1
Steven van Zelst <STUDENTNUMMER@talnet.nl>
2020-02-06 18:22:27 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,24 @@
+use netland;
+SELECT id, title, rating, description, has_won_awards, seasons, country, language
+from
+	series
+-- WHERE
+--     has_won_awards = 1;
+-- WHERE
+--     rating > 2.5;
+-- WHERE
+--     country = "NL" and
+--     language = "NL";
+-- WHERE
+--     country = "NL" and
+--     language = "NL";
+-- WHERE
+--     seasons < 5;
+-- where
+-- 	rating = 5;
+-- where
+-- 	seasons < 3 or seasons> 20;
+-- where
+-- 	title like "%th%";
+-- where
+-- 	seasons not like 3;
